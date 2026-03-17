ccfa0937b40133b9e562b74ccc465aab4bb4241a
Jagdish Uikey <juikey697@gmail.com>
2026-03-17 20:55:46 +0530

    done necessary changes for deployment

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const httpServer = createServer(app)
 // Socket.IO setup
 const io = new Server(httpServer, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: 'https://campus-connect-kohl.vercel.app/' || 'http://localhost:5173',
     methods: ['GET', 'POST'],
     credentials: true,
   }
@@ -95,7 +95,7 @@ io.on('connection', (socket) => {
 
 // CORS Middleware - must be before routes
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://campus-connect-kohl.vercel.app/' || 'http://localhost:5173',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
